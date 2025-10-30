5093f060f3dbc126fb0c355fb280e0073fdf9959
Naveen-Tamil477 <naveenprasath.t@bt.iith.ac.in>
2025-10-30 08:34:56 +0000

    update homepage and images

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/department_website/',
+  base: '/department_website/',  // IMPORTANT for GitHub Pages
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
